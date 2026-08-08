dbfd01571b1ea738d06f57e32fc6d23010e9aebe
Maksim Obroskov <CAPITANRB-107@yandex.ru>
2026-08-08 20:12:30 +0300

    feat: добавлено чтение CSV и Excel файлов
    - Создан модуль src/reading.py
    - еализована функция read_transactions_from_csv
    - еализована функция read_transactions_from_excel
    - обавлены зависимости pandas и openpyxl
    - обавлены тесты для чтения CSV и Excel
    - се 16 тестов проходят
    - окрытие кода: 93%
    
    Функции:
    - read_transactions_from_csv - чтение CSV с транзакциями
    - read_transactions_from_excel - чтение Excel с транзакциями

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,14 @@
 """Пакет src для банковского виджета."""
 
+from src.constants import (
+    ACCOUNT_NUMBER_MIN_LENGTH,
+    CANCELED_STATUS,
+    CARD_NUMBER_LENGTH,
+    EXECUTED_STATUS,
+    PENDING_STATUS,
+)
 from src.masks import get_mask_account, get_mask_card_number
+from src.reading import read_transactions_from_csv, read_transactions_from_excel
 from src.widget import get_date, mask_account_card
 
 __all__ = [
@@ -8,4 +16,11 @@ __all__ = [
     "get_mask_card_number",
     "mask_account_card",
     "get_date",
+    "read_transactions_from_csv",
+    "read_transactions_from_excel",
+    "EXECUTED_STATUS",
+    "CANCELED_STATUS",
+    "PENDING_STATUS",
+    "CARD_NUMBER_LENGTH",
+    "ACCOUNT_NUMBER_MIN_LENGTH",
 ]
